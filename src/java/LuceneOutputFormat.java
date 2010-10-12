d0d48cad6246112bbc19808a77519a23f4b63555
Aaron Binns <aaron@archive.org>
2010-10-12 16:53:37 -0700

    Lots of custom code for adding documents to the index according to various rules and policies.

--- src/java/LuceneOutputFormat.java
+++ src/java/LuceneOutputFormat.java
@@ -31,6 +31,29 @@ public class LuceneOutputFormat extends FileOutputFormat<Text, MapWritable>
   public Path perm;
   
   public IndexWriter writer;
+
+  public Set<String> allowedContentTypes = new HashSet<String>( );
+  public Map<String,String> contentTypeAliases = new HashMap<String,String>( );
+  {
+    // PDF aliases.
+    contentTypeAliases.put( "application/x-pdf", "application/pdf" );
+
+    // HTML aliases.
+    contentTypeAliases.put( "application/xhtml+xml", "text/html" );
+
+    // MS Word aliases.
+    contentTypeAliases.put( "application/vnd.ms-word", "application/msword" );
+    contentTypeAliases.put( "application/vnd.openxmlformats-officedocument.wordprocessingml.document", "application/msword" );
+
+    // PowerPoint aliases.
+    contentTypeAliases.put( "application/mspowerpoint",     "application/vnd.ms-powerpoint" );
+    contentTypeAliases.put( "application/ms-powerpoint",    "application/vnd.ms-powerpoint" );
+    contentTypeAliases.put( "application/mspowerpnt",       "application/vnd.ms-powerpoint" );
+    contentTypeAliases.put( "application/vnd-mspowerpoint", "application/vnd.ms-powerpoint" );
+    contentTypeAliases.put( "application/powerpoint",       "application/vnd.ms-powerpoint" );
+    contentTypeAliases.put( "application/x-powerpoint",     "application/vnd.ms-powerpoint" );
+    contentTypeAliases.put( "application/vnd.openxmlformats-officedocument.presentationml.presentation", "application/vnd.ms-powerpoint" );
+  }
   
   public RecordWriter<Text, MapWritable> getRecordWriter( FileSystem notUsed,
                                                           JobConf job,
@@ -38,11 +61,21 @@ public class LuceneOutputFormat extends FileOutputFormat<Text, MapWritable>
                                                           Progressable progress )
     throws IOException
   {
+    // Populate our list of allowed Content-Types
+    String types = job.get( "indexer.allowedContentTypes", "application/msword application/pdf application/vnd.ms-powerpoint text/html text/plain" );
+
+    for ( String type : types.split( "\\s+" ) )
+      {
+        if ( type.length() < 1 ) continue;
+
+        allowedContentTypes.add( type );
+      }
+
     // Open Lucene index in ${temp}
     this.fs = FileSystem.get(job);
 
     perm = new Path(FileOutputFormat.getOutputPath(job), name);
-    temp = job.getLocalPath("index/_"  + (new Random().nextInt()));
+    temp = job.getLocalPath( "index/_"  + (new Random().nextInt()) );
 
     fs.delete(perm, true); // delete old, if any
     writer = new IndexWriter( new NIOFSDirectory( new File( fs.startLocalOutput( perm, temp ).toString( ) ) ),
@@ -54,11 +87,11 @@ public class LuceneOutputFormat extends FileOutputFormat<Text, MapWritable>
     writer.setMaxBufferedDocs(job.getInt("indexer.minMergeDocs", 100));
     writer.setMaxMergeDocs(job.getInt("indexer.maxMergeDocs", Integer.MAX_VALUE));
     writer.setTermIndexInterval(job.getInt("indexer.termIndexInterval", 128));
-    writer.setMaxFieldLength(job.getInt("indexer.max.tokens", 10000));
-    writer.setInfoStream(LogUtil.getDebugStream(Indexer.LOG));
-    writer.setUseCompoundFile(false);
     writer.setSimilarity(new NutchSimilarity());
     */
+
+    writer.setMaxFieldLength( job.getInt("indexer.max.tokens", Integer.MAX_VALUE) );
+    writer.setUseCompoundFile(false);
     
     LuceneRecordWriter w = new LuceneRecordWriter( );
 
@@ -70,25 +103,84 @@ public class LuceneOutputFormat extends FileOutputFormat<Text, MapWritable>
     public void write( Text key, MapWritable map )
       throws IOException
     {
-      // TODO: Put the document into the Lucene index.
       Document doc = new Document();
       
       MapAdapter a = new MapAdapter( map );
 
-      // Only do text/html for now.
-      if ( ! "text/html".equals( a.get( "type" ) ) )
+      // Skip if not an allowed type.
+      String type = a.get( "type" );
+
+      // First, canonicalize the type name.
+      if ( contentTypeAliases.containsKey( type ) )
+        {
+          type = contentTypeAliases.get( type );
+        }
+
+      // If there are any explicitly allowed content-types, then this
+      // has to be one of them.
+      if ( allowedContentTypes.size() > 0 && ! allowedContentTypes.contains( type ) )
         {
           return ;
         }
 
-      doc.add( new Field( "url",        a.get("url"),            Field.Store.YES,      Field.Index.NO ) );
-      doc.add( new Field( "title",      a.get("title"),          Field.Store.YES,      Field.Index.ANALYZED) );
-      doc.add( new Field( "content",    a.get("content_parsed"), Field.Store.COMPRESS, Field.Index.ANALYZED) );
-      doc.add( new Field( "collection", a.get("collection"),     Field.Store.YES,      Field.Index.NOT_ANALYZED_NO_NORMS) );
-      doc.add( new Field( "type",       a.get("type"),           Field.Store.YES,      Field.Index.NOT_ANALYZED_NO_NORMS) );
-      doc.add( new Field( "length",     a.get("length"),         Field.Store.YES,      Field.Index.NO) );
-      doc.add( new Field( "date",       a.get("date"),           Field.Store.YES,      Field.Index.NO) );
+      // Skip if text/plain and /robots.txt
+      try
+        {
+          URI uri = new URI( a.get("url") );
+          if ( "text/plain".equals( type ) && "/robots.txt".equals( uri.getPath() ) )
+            {
+              return ;
+            }
+        } catch ( URISyntaxException e ) { }
+
+      // TODO: Some heuristic to detect, and skip, bogus text/plain files.  Like video/flash/etc.
+      //       mis-labeled as text/plain.
+
+      // Special handling for type.  Index on the canonicalized type, but store the original type.
+      /*
+      doc.add( new Field( "type", type,          Field.Store.NO,  Field.Index.NOT_ANALYZED_NO_NORMS ) );
+      doc.add( new Field( "type", a.get("type"), Field.Store.YES, Field.Index.NO ) );
+      */
+      // Nahh, just use the canonicalized type for both.
+      doc.add( new Field( "type", type, Field.Store.YES, Field.Index.NOT_ANALYZED_NO_NORMS ) );
       
+      doc.add( new Field( "url",        a.get("url"),        Field.Store.YES, Field.Index.NO                    ) );
+      doc.add( new Field( "title",      a.get("title"),      Field.Store.YES, Field.Index.ANALYZED              ) );
+      doc.add( new Field( "collection", a.get("collection"), Field.Store.YES, Field.Index.NOT_ANALYZED_NO_NORMS ) );
+      doc.add( new Field( "length",     a.get("length"),     Field.Store.YES, Field.Index.NO                    ) );
+
+      // Special handling for content field.
+      String content = a.get( "content_parsed" );
+      if ( content.length() > 0 )
+        {
+          doc.add( new Field( "content", content, Field.Store.NO, Field.Index.ANALYZED ) );
+
+          byte[] compressed = CompressionTools.compressString( content );
+
+          // Store the shorter of the two.
+          if ( compressed.length < content.length() )
+            {
+              doc.add( new Field( "content", compressed, Field.Store.YES ) );    
+            }
+          else
+            {
+              doc.add( new Field( "content", content, Field.Store.YES, Field.Index.NO ) );
+            }
+        }
+
+      // Special handling for dates.
+      String date = a.get("date");
+      if ( date.length() == "yyyymmddhhmmss".length( ) )
+        {
+          // Store, but do not index, the full 14-character date.
+          doc.add( new Field( "date", date,                   Field.Store.YES, Field.Index.NO  ) );
+
+          // Index, but do not store, the year and the year+month.  These are what can be searched.
+          doc.add( new Field( "date", date.substring( 0, 4 ), Field.Store.NO,  Field.Index.NOT_ANALYZED_NO_NORMS ) );
+          doc.add( new Field( "date", date.substring( 0, 6 ), Field.Store.NO,  Field.Index.NOT_ANALYZED_NO_NORMS ) );
+        }
+
+      // Special handling for site
       String url = a.get( "url" );
       try
         {
@@ -100,8 +192,7 @@ public class LuceneOutputFormat extends FileOutputFormat<Text, MapWritable>
               site = site.substring( 4 );
             }
 
-          doc.add( new Field( "site", site, Field.Store.YES, Field.Index.NOT_ANALYZED_NO_NORMS) );
-
+          doc.add( new Field( "site", site, Field.Store.NO, Field.Index.NOT_ANALYZED_NO_NORMS) );
         }
       catch ( MalformedURLException mue )
         {
@@ -147,6 +238,6 @@ class MapAdapter
 
     if ( value == null ) return "";
 
-    return value.toString();
+    return value.toString().trim();
   }
 }
