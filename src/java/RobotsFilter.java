94c8905e530ed8c790e2f3a52c951bc05643192b
Aaron Binns <aaron@archive.org>
2010-10-29 13:28:22 -0700

    Ignore type, just exclude /robots.txt and /favicon.ico.

--- src/java/RobotsFilter.java
+++ src/java/RobotsFilter.java
@@ -9,13 +9,16 @@ public class RobotsFilter implements DocumentFilter
 
   public boolean isAllowed( DocumentProperties properties )
   {
-    String type = properties.get( "type" );
     String url  = properties.get( "url"  );
     
     try
       {
         URI uri = new URI( url );
-        if ( "text/plain".equals( type ) && "/robots.txt".equals( uri.getPath() ) )
+        
+        String path = uri.getPath().trim( );
+
+        if ( "/favicon.ico".equals( path ) ||
+             "/robots.txt" .equals( path ) )
           {
             return false;
           }
