6c70dc7aa6e616bc795ff130ae2b019a9cdcd572
Aaron Binns <aaron@archive.org>
2010-11-28 20:35:34 -0800

    Added MAX_BODY_LENGTH limiter to String before indexing it.

--- src/java/BodyHandler.java
+++ src/java/BodyHandler.java
@@ -9,26 +9,35 @@ import org.apache.lucene.index.*;
 
 public class BodyHandler implements FieldHandler
 {
+  int MAX_BODY_LENGTH = 100000;
 
   public void handle( Document doc, DocumentProperties properties )
   {
     // Special handling for content field.
     String content = properties.get( "content_parsed" );
-    if ( content.length() > 0 )
+
+    if ( content.length() < 1 )
       {
-        doc.add( new Field( "content", content, Field.Store.NO, Field.Index.ANALYZED ) );
+        return ;
+      }
 
-        byte[] compressed = CompressionTools.compressString( content );
-
-        // Store the shorter of the two.
-        if ( compressed.length < content.length() )
-          {
-            doc.add( new Field( "content", compressed, Field.Store.YES ) );    
-          }
-        else
-          {
-            doc.add( new Field( "content", content, Field.Store.YES, Field.Index.NO ) );
-          }
+    if ( content.length( ) > MAX_BODY_LENGTH )
+      {
+        content = content.substring( 0, MAX_BODY_LENGTH );
+      }
+    
+    doc.add( new Field( "content", content, Field.Store.NO, Field.Index.ANALYZED ) );
+    
+    byte[] compressed = CompressionTools.compressString( content );
+    
+    // Store the shorter of the two.
+    if ( compressed.length < content.length() )
+      {
+        doc.add( new Field( "content", compressed, Field.Store.YES ) );    
+      }
+    else
+      {
+        doc.add( new Field( "content", content, Field.Store.YES, Field.Index.NO ) );
       }
   }
 
